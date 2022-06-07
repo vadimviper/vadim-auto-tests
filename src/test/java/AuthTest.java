478b0c9b325508bb89c568088ea452e156c8b9f3
Vipervadim <v.habibrahmanov.kazanexpress@gmail.com>
2022-06-07 09:39:31 +0300

    auto-test-junit-add-step

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -1,16 +1,24 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.Keys;
 
 import java.util.stream.Stream;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+@Owner("vipervadim")
+@Feature("Авторизация")
 public class AuthTest {
 
     @BeforeEach
@@ -21,14 +29,18 @@ public class AuthTest {
     }
 
     @Test
-    @DisplayName("Успешная атворизация")
+    @Story("Успешная авторизация")
+    @DisplayName("Авторизация с корректными логином и паролем")
     public void shouldAuthorizeTest() {
+        step("Ввести логин и пароль и нажать кнопку 'Войти'",() -> {
         TestPages.mainPage.loginField()
                 .sendKeys("v.habibrahmanov.kazanexpress@gmail.com");
         TestPages.mainPage.passwordField()
                 .sendKeys("Vipervadim32");
         TestPages.mainPage.loginButton()
                 .click();
+        });
+        step("Авторизация прошла успешно", () -> {
         TestPages.mainPage.headerBanner()
                 .shouldBe(visible);
         TestPages.mainPage.dropdownButton()
@@ -37,20 +49,27 @@ public class AuthTest {
                 .click();
         TestPages.mainPage.headerName()
                 .shouldBe(visible);
+    });
     }
 
     @MethodSource("incorrectCredentials")
     @ParameterizedTest(name = "{displayName} :{0}")
+    @Story("Авторизация с некорректными данными")
     @DisplayName("Авторизация с некорректными данными:")
     public void shouldNotAuthorizeTest(String type, String phone, String password){
+        step("Ввести некорректные данные", () -> {
         TestPages.mainPage.loginField()
                 .sendKeys(phone);
         TestPages.mainPage.passwordField()
                 .sendKeys(password);
         TestPages.mainPage.loginButton()
                 .click();
+        });
+
+        step("Авторизация не проходит. Отображается сообщение об ошибке", () -> {
         TestPages.mainPage.warningPopup()
                 .shouldBe(visible);
+        });
     }
     static Stream<Arguments> incorrectCredentials() {
         return Stream.of(
--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,3 +1,6 @@
+import io.qameta.allure.Owner;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,50 +15,76 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.sleep;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+@Owner("vipervadim")
+@Feature("Проверка репозиториев 'ЮнитТим'")
 public class JUnitTest {
 
     @BeforeEach
     public void setup(){
+        step("Открыть сайт https://github.com/junit-team", () -> {
         open("https://github.com/junit-team");
         TestPages.JUnitPage.repositoriesButton()
                 .click();
         TestPages.JUnitPage.jUnitTeamImage()
                 .shouldBe(visible);
+        });
     }
 
     @Test
+    @Story("Проверка репозитория 'Юнит4'")
     @DisplayName("Выбор ветки")
     public void selectionBranchFixtures() {
+        step("Открыть репозиторий 'ДжейЮнит4'", () -> {
         TestPages.JUnitPage.jUnitButton()
                 .click();
         TestPages.JUnitPage.jUnitText()
                 .shouldBe(visible);
+        });
+
+        step("Открыть дропдаун и переключиться на ветку 'Фикстура'", () -> {
         TestPages.JUnitPage.dropdownJUnitButton()
                 .click();
         TestPages.JUnitPage.fixturesBranchButton()
                 .click();
+        });
+
+        step("Проверить, что произошло переключение на ветку 'Фикстура'", () -> {
         TestPages.JUnitPage.fixturesTitle()
                 .shouldBe(visible);
+        });
     }
 
     @MethodSource("searchBy")
     @ParameterizedTest(name = "{displayName} :{0}")
+    @Story("Проверка релизов в репозитории 'Юнит4'")
     @DisplayName("Позитивня проверка поиска по релизам в репозитории")
     public void positiveSearch(String type, String searchData, String searchResults){
+        step("Открыть репозиторий 'ДжейЮнит4'", () -> {
         TestPages.JUnitPage.jUnitButton()
                 .click();
         TestPages.JUnitPage.jUnitText()
                 .shouldBe(visible);
+        });
+
+        step("Перейти в раздел 'Релизы'", () -> {
         TestPages.JUnitPage.releaseButton()
                 .click();
         TestPages.JUnitPage.releasesPage()
                 .shouldBe(visible);
+        });
+
+        step("Ввести данные в поле и произвести поиск", () -> {
         TestPages.JUnitPage.releaseSearch()
                 .sendKeys(searchData + Keys.ENTER);
+        });
+
+        step("Проверить, что поиск прошёл успешно", () -> {
         TestPages.JUnitPage.releasesBlock()
                 .shouldHave(text(searchResults));
+        });
     }
 
     static Stream<Arguments> searchBy() {
